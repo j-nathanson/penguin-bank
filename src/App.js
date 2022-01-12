af1fdb774b31d30528db0fccbfab2b34756600ac
jacobnathanson <jacobnath93@gmail.com>
2022-01-12 01:14:23 -0500

    conect ui to dispatch to action creators

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/action-creators";
 
 function App() {
 
   // return the state inside the store
   // can do things like state.account to just get that state data
-  const account = useSelector(state => state.account)
+  const account = useSelector(state => state.account);
+
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch)
   return (
     <div className="App">
       <h1>{account}</h1>
-      <button>Deposit</button>
-      <button>Withdraw</button>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
     </div>
   );
 }
